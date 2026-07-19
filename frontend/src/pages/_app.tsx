826f474ef066e812ffc41203e47f25f370e44354
unknown <shikharshah30@gmail.com>
2026-07-20 00:12:34 +0530

    refactor: metadata changes

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -15,7 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Head>
-        <html lang="en" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0b1326" />
